8fc8d634267a9ce34701166885e76b7be3c7a2da
Sudhi-Varun <sudhivarun1988@gmail.com>
2025-11-20 07:57:04 +0530

    Revert "updated"
    This reverts commit 4fef8a6c9d8b875e39bad6ed78f292b4afa33824.

--- modal-manager.js
+++ modal-manager.js
@@ -1,13 +1,5 @@
 MicroModal.init();
 
-document.addEventListener('micromodal:show', function () {
-  document.body.classList.add('modal-blur');
-});
-
-document.addEventListener('micromodal:close', function () {
-  document.body.classList.remove('modal-blur');
-});
-
 setTimeout(function(){
   MicroModal.show('report-modal');
 }, 5000);
