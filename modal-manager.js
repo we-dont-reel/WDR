4fef8a6c9d8b875e39bad6ed78f292b4afa33824
Sudhi-Varun <sudhivarun1988@gmail.com>
2025-11-20 07:51:54 +0530

    updated

--- modal-manager.js
+++ modal-manager.js
@@ -1,5 +1,13 @@
 MicroModal.init();
 
+document.addEventListener('micromodal:show', function () {
+  document.body.classList.add('modal-blur');
+});
+
+document.addEventListener('micromodal:close', function () {
+  document.body.classList.remove('modal-blur');
+});
+
 setTimeout(function(){
   MicroModal.show('report-modal');
 }, 5000);
