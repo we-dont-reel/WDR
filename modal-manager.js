683aab56e453b70659d5778763e1cba2c7669b32
Sudhi-Varun <sudhivarun1988@gmail.com>
2025-11-20 19:04:55 +0530

    updated

--- modal-manager.js
+++ modal-manager.js
@@ -1,18 +1,11 @@
 MicroModal.init();
 
-setTimeout(function(){
+setInterval(function(){
   MicroModal.show('report-modal');
 }, 5000);
 
 function closeModal(id){
-  const modal=document.getElementById(id);
-  const overlay=modal.querySelector('.modal__overlay');
-  const container=modal.querySelector('.modal__container');
-  overlay.classList.add('fadeout');
-  container.classList.add('fadeout');
   setTimeout(function(){
-    overlay.classList.remove('fadeout');
-    container.classList.remove('fadeout');
     MicroModal.close(id);
-  },300);
+  }, 100);
 }
