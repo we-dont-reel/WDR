3c12680580979260eb9ddbebe21426a2ece6066a
Sudhi-Varun <sudhivarun1988@gmail.com>
2025-11-20 09:25:02 +0530

    Revert "updated"
    This reverts commit aaca61f494b348e8220a553900ae24832310d987.

--- modal-manager.js
+++ modal-manager.js
@@ -1,16 +1,4 @@
-MicroModal.init({
-  onShow: function(modal) {
-    document.body.classList.add('modal-blur');
-    modal.querySelector('.modal__container').classList.add('modal-ease-active');
-  },
-  onClose: function(modal) {
-    document.body.classList.remove('modal-blur');
-    modal.querySelector('.modal__container').classList.remove('modal-ease-active');
-  }
-});
-
-document.querySelector('#report-modal .modal__container').classList.add('modal-ease');
-
+MicroModal.init();
 
 setTimeout(function(){
   MicroModal.show('report-modal');
