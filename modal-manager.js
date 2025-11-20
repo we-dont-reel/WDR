aaca61f494b348e8220a553900ae24832310d987
Sudhi-Varun <sudhivarun1988@gmail.com>
2025-11-20 08:03:55 +0530

    updated

--- modal-manager.js
+++ modal-manager.js
@@ -1,4 +1,16 @@
-MicroModal.init();
+MicroModal.init({
+  onShow: function(modal) {
+    document.body.classList.add('modal-blur');
+    modal.querySelector('.modal__container').classList.add('modal-ease-active');
+  },
+  onClose: function(modal) {
+    document.body.classList.remove('modal-blur');
+    modal.querySelector('.modal__container').classList.remove('modal-ease-active');
+  }
+});
+
+document.querySelector('#report-modal .modal__container').classList.add('modal-ease');
+
 
 setTimeout(function(){
   MicroModal.show('report-modal');
