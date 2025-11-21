828542b8c9bd2040a3a827c6270e9cd4a3bcfdf1
Sudhi-Varun <sudhivarun1988@gmail.com>
2025-11-21 13:21:22 +0530

    updated

--- modal-manager.js
+++ modal-manager.js
@@ -4,6 +4,13 @@ setInterval(function(){
   MicroModal.show('share-modal');
 }, 5000);
 
+function copyShareText(textareaId){
+  var el = document.getElementById(textareaId);
+  el.select();
+  el.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(el.value);
+}
+
 function closeModal(id){
   setTimeout(function(){
     MicroModal.close(id);
