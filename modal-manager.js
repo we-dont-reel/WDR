337d50739519c2b13d9bb9b3e71b20b888d5dddd
Sudhi-Varun <sudhivarun1988@gmail.com>
2025-11-21 16:38:41 +0530

    updated

--- modal-manager.js
+++ modal-manager.js
@@ -7,17 +7,24 @@ setInterval(function(){
 function copyShareText(textareaId){
   var el=document.getElementById(textareaId);
   var btn=event.target;
+  var oldText=btn.innerHTML;
   navigator.clipboard.writeText(el.value).then(function(){
     el.blur();
     window.getSelection().removeAllRanges();
     btn.innerText="Copied...";
-    setTimeout(function(){btn.innerText="Copy";},1200);
+    setTimeout(function(){
+      btn.innerHTML=oldText;
+      MicroModal.close('share-modal');
+    },500);
   }).catch(function(){
     navigator.clipboard.writeText(el.value);
     el.blur();
     window.getSelection().removeAllRanges();
     btn.innerText="Copied...";
-    setTimeout(function(){btn.innerText="Copy";},1200);
+    setTimeout(function(){
+      btn.innerHTML=oldText;
+      MicroModal.close('share-modal');
+    },500);
   });
 }
 
